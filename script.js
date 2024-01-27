2bc532da6e00564d2822b7bced63f7dc47355826
Prashant Srivastav <srivastavp891@gmail.com>
2024-01-27 18:22:16 +0530

    snake svg fix

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-const converter = new showdown.Converter();
-fetch('./README.md')
-  .then(response => response.text())
-  .then(markdown => {
-    const html = converter.makeHtml(markdown);
-    document.getElementById('readme-content').innerHTML = html;
-  });
